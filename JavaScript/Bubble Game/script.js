a7e1e53047f39298d74abc119d9a783e925a16b7
siddharth rukadikar <siddharthrukadikar@gmail.com>
2024-09-01 20:07:34 +0530

    bubble game using js

---
+++ JavaScript/Bubble Game/script.js
@@ -0,0 +1,48 @@
+function MakeBubble() {
+    var clutter=""
+
+for(var i=1; i<=207; i++){
+    var rn=Math.floor(Math.random()*10)
+    clutter+=`<div class="bubble w-[3vw] h-[6vh] flex items-center justify-center cursor-pointer text-white rounded-[50%] bg-green-900">${rn}</div>`
+}
+
+document.querySelector('.pbtm').innerHTML=clutter
+}
+
+var timer=60;
+function Timer(){
+    var time=setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector(".Timer").textContent=timer
+        }else{
+            clearInterval(timer)
+            document.querySelector(".pbtm").innerHTML=`<div class="flex items-center w-full justify-center">Game Over</div>`
+        }
+    },1000)
+}
+var Hit_rn=undefined;
+function getNewHit() {
+     Hit_rn=Math.floor(Math.random()*10)
+    document.querySelector(".Hit").textContent=Hit_rn
+}
+
+let score=0;
+function GetScore() {
+    score+=10;
+    document.querySelector(".score").textContent=score
+}
+
+document.querySelector(".pbtm").addEventListener('click', function(e){
+    if(Number(e.target.textContent) === Hit_rn ){
+        GetScore()
+        getNewHit()
+        MakeBubble()
+    }
+    
+})
+
+MakeBubble()
+
+Timer()
+getNewHit()
