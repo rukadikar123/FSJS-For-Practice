19dacf5be4d14b3f8c179fbe516bbd584e0118de
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-02-13 17:38:09 +0530

    add Closure practice que

--- JavaScript/logic_building/03.js
+++ JavaScript/logic_building/03.js
@@ -12,20 +12,39 @@
 // console.log(reverseString('siddharth'));
 
 
-function reversestring(str){
-    let result=""
-    if (typeof(str)==="string") {
-        for(i=str.length-1 ; i>=0 ; i--){
-            result+=str[i]
+// function reversestring(str){
+//     let result=""
+//     if (typeof(str)==="string") {
+//         for(i=str.length-1 ; i>=0 ; i--){
+//             result+=str[i]
             
-        }
-    }else {
-        throw new Error("only strings are allowed");
+//         }
+//     }else {
+//         throw new Error("only strings are allowed");
+        
+//     }
+//     return result
+// }
+
+// console.log(reversestring("siddharth"));
+
+// console.log(reversestring(12425));
+
+
+function createGreet(greet){
+    let count=0;
+    return function inner(Name){
+        count++;
+        console.log(`${greet} ${Name} and count is ${count}`);
         
     }
-    return result
 }
 
-console.log(reversestring("siddharth"));
+const firstVar=createGreet("hi")
+const secondVar=createGreet("hello")
 
-console.log(reversestring(12425));
+firstVar("sid")
+firstVar("sid")
+firstVar("sid")
+secondVar("rahul")
+firstVar("sid")
