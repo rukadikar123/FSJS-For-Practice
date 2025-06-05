b4fb78e321b57d9d60ce718e2f8899d00de8e197
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-06-05 11:25:21 +0530

    refactor: update counter logic in App component and add additional examples

--- JavaScript/logic_building/18.js
+++ JavaScript/logic_building/18.js
@@ -153,3 +153,78 @@
 //   }
 
 // console.log(result);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+
+// let Input= [1, [2, [3, [4]]]]
+// let flattedArray=Input.flat(Infinity)
+
+// console.log(flattedArray);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// 6. Find Duplicate Elements
+
+// let Input= [1, 2, 3, 2, 4, 5, 1]
+
+// let count={}
+// let result=[]
+
+// for(let item of Input ){
+//     count[item]=(count[item] || 0) + 1
+    
+// }
+// Object.keys(count).forEach((key)=>{
+//     if(count[key]>1){
+//         result.push(+key)
+//     }
+// })
+
+// console.log(result);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// 7. Longest Word in a Sentence
+
+
+// let Input= "Web development is awesome"
+
+// let splittedStr=Input.split(" ")
+// console.log(splittedStr);
+
+// let count={}
+
+// for(let word of splittedStr){
+//     count[word]=word.length
+// }
+// console.log(count);
+
+// let sortedArray=Object.values(count).sort((a,b)=> b-a)
+
+// let greatestValue=sortedArray[0]
+
+// let result=Object.keys(count).filter((key)=> count[key]===greatestValue )
+
+// console.log(result);
+
+
+// let Input= "Web development is awesome"
+// let words=Input.split(" ")
+
+// let maxLength=0;
+// let result=[]
+
+// for(let word of words){
+//     if(word.length>maxLength){
+//         maxLength=word.length;
+//         result=[word]
+//     }else if(word.length===maxLength){
+//         result.push(word)
+//     }
+// }
+
+// console.log(result);
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
--- reactPractice/02counter/src/App.jsx
+++ reactPractice/02counter/src/App.jsx
@@ -3,24 +3,25 @@ import { useState } from "react"
 function App() {
 // let counter=5
 let [counter, setCounter]= useState(5)
-const addValue=()=>{
-   if(counter<20){
-    counter=counter+1
-   }
-   setCounter(counter)
-}
+// const addValue=()=>{
+// //    if(counter<20){
+// //     counter=counter+1
+// //    }
+// //    setCounter(counter)
+// setCounter((prev)=>prev+1)
+// }
 const decreaseValue=()=>{
   if(counter>0){
-    counter=counter-1
+    setCounter(counter=>counter-1)
   }
-  setCounter(counter)
+  
 }
   return (
     <>
       <h1>chai aur react</h1>
       <h2>counter value: {counter}</h2>
       <button
-      onClick={addValue}
+      onClick={()=>setCounter(prev=>prev+1)}
       >Add value</button>
       <br /> 
       <button
